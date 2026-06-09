0e396cb5b9efa425fe3385b329dfaede6dd1fc2f
unknown <renan.workup@gmail.com>
2026-06-09 20:44:12 -0300

    Terceira questão

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ app.listen(
     3000, 
     () => console.log(`Servidor em execução`)
 );
-//parte dois: criação dos manos gets, questão 2
+//parte dois: criação dos primeiro mano get, questão 2
 
 app.get('/sobre', (req, res)=>{
   res.send('Oi, estou louca ao completar esse trabalho, me perdoe, é estranho mexer no git, é desconfortável...')
+})
+//Quesão 3:
+app.get ('/contato', (req, res)=>{
+  res.send({
+    "email": 'contato@exemplo.com',
+    "telefone": '(81) 99999-9999'
+  })
 })
